805a85a8a7fe02c25bbfc4edd38837beb684abc6
枕舟泊 <3268054739@qq.com>
2026-05-15 17:21:53 +0800

    全删

--- backend/src/main/java/com/qms/carbon/Mapper/CarbonFactorLogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.qms.carbon.Mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.qms.carbon.Entity.CarbonFactorLog;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CarbonFactorLogMapper extends BaseMapper<CarbonFactorLog> {
-}
--- backend/src/main/java/com/qms/carbon/Service/AiService.java
+++
@@ -1,8 +0,0 @@
-package com.qms.carbon.Service;
-
-public interface AiService {
-    /**
-     * 获取 AI 低碳建议
-     */
-    String getGreenAdvice(String userContent);
-}
